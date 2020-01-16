2141d157ea724ed5a41d3efd6719527f173d5c89
abhinav2091 <abhinavsharma043@gmail.com>
2020-01-16 17:01:02 +0530

    initial commit

---
+++ Springboot-Blogging-App/src/main/java/com/luv2code/springboot/Springboot/Blogging/App/SpringbootBloggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.Springboot.Blogging.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBloggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBloggingAppApplication.class, args);
+	}
+
+}
---
+++ Springboot-Blogging-App/src/test/java/com/luv2code/springboot/Springboot/Blogging/App/SpringbootBloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.Springboot.Blogging.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
